12feba65fdae0a172526c399ff2cdbe33cf812e7
Lupo Montero <lupomontero@gmail.com>
2015-02-12 20:57:43 -0500

    oops

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 
 gulp.task('lint', function () {
-  return gulp.src([ 'Gruntfile.js', 'index.js', 'test/*.spec.js' ])
+  return gulp.src([ 'gulpfile.js', 'index.js', 'test/*.spec.js' ])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
